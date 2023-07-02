65f36e671b5227ea84de26758b947aa3b3417140
Parker Barrett <parkerb7@vt.edu>
2023-07-02 14:55:43 -0400

    Create skeleton file structure

---
+++ include/Quaternion.hpp
@@ -0,0 +1,23 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                         Quaternion Data Type                                     //
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                  // 
+// Author:      Parker Barrett                                                                      //
+// Overview:    Header file for the quaternion class.                                               //      //                                                                                                  //
+//                                                                                                  //
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Include Headers
+#pragma once
+#include <iostream>
+#include <cmath>
+
+// Quaternion Class
+class Quaternion {
+
+    // Public Class Members/Functions
+    public:
+
+    // Private Class Members/Function
+    private:
+};
