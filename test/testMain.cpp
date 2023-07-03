3cb147f788ff4e61f842a4607974c2616cec86a4
Parker Barrett <parkerb7@vt.edu>
2023-07-03 06:04:59 -0400

    Updates to .clang-format

--- test/testMain.cpp
+++ test/testMain.cpp
@@ -9,9 +9,9 @@
 
 // Main Function
 int main(int argc, char **argv) {
-  // Initialize Google Test
-  testing::InitGoogleTest(&argc, argv);
+    // Initialize Google Test
+    testing::InitGoogleTest(&argc, argv);
 
-  // Run Tests
-  return RUN_ALL_TESTS();
+    // Run Tests
+    return RUN_ALL_TESTS();
 }
