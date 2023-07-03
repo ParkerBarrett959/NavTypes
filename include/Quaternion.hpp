bbde78c063d07dd008161ae0c5a197121c2bd4b3
Parker Barrett <parkerb7@vt.edu>
2023-07-03 06:05:28 -0400

    Quaternion class constructor and getters

--- include/Quaternion.hpp
+++ include/Quaternion.hpp
@@ -11,8 +11,59 @@
 
 // Quaternion Class
 class Quaternion {
-  // Public Class Members/Functions
- public:
-  // Private Class Members/Function
- private:
+  public:
+    /* @Quaternion
+     * @Description: Quaternion class constructor which sets values of
+     * quaternion elements.
+     * @Inputs:
+     *     q0: Scalar quaternion element
+     *     q1: First vector quaternion element
+     *     q2: Second vector quaternion element
+     *     q3: Third vector quaternion element
+     * @Outputs:
+     */
+    Quaternion(double q0, double q1, double q2, double q3) : q0_(q0), q1_(q1), q2_(q2), q3_(q3){};
+
+    /* @Quaternion
+     * @Description: Quaternion class default constructor.
+     * @Inputs:
+     * @Outputs:
+     */
+    Quaternion() : q0_(0.0), q1_(0.0), q2_(0.0), q3_(1.0){};
+
+    /* @getQ0
+     * @Description: Returns scalar element of quaternion.
+     * @Inputs:
+     * @Outputs:
+     *     q0_: Scalar element of quaternion
+     */
+    double getQ0() { return q0_; };
+
+    /* @getQ1
+     * @Description: Returns first vector element of quaternion.
+     * @Inputs:
+     * @Outputs:
+     *     q1_: First vector element of quaternion
+     */
+    double getQ1() { return q1_; };
+
+    /* @getQ2
+     * @Description: Returns second vector element of quaternion.
+     * @Inputs:
+     * @Outputs:
+     *     q2_: Second vector element of quaternion
+     */
+    double getQ2() { return q2_; };
+
+    /* @getQ3
+     * @Description: Returns third vector element of quaternion.
+     * @Inputs:
+     * @Outputs:
+     *     q3_: Third vector element of quaternion
+     */
+    double getQ3() { return q3_; };
+
+  private:
+    // Quaternion Elements
+    double q0_, q1_, q2_, q3_;
 };
--- test/QuaternionTest.cpp
+++ test/QuaternionTest.cpp
@@ -9,3 +9,27 @@
 
 // External Includes
 #include "gtest/gtest.h"
+
+// Test Constructor
+TEST(Constructor, Standard) {
+    // Construct Quaternion
+    Quaternion q(1.0, 2.0, 3.0, 4.0);
+
+    // Assertions
+    EXPECT_EQ(q.getQ0(), 1.0);
+    EXPECT_EQ(q.getQ1(), 2.0);
+    EXPECT_EQ(q.getQ2(), 3.0);
+    EXPECT_EQ(q.getQ3(), 4.0);
+}
+
+// Test Default Constructor
+TEST(Constructor, Default) {
+    // Construct Quaternion
+    Quaternion q;
+
+    // Assertions
+    EXPECT_EQ(q.getQ0(), 0.0);
+    EXPECT_EQ(q.getQ1(), 0.0);
+    EXPECT_EQ(q.getQ2(), 0.0);
+    EXPECT_EQ(q.getQ3(), 1.0);
+}
