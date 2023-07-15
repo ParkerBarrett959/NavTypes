59565d725cc5df2fc1ed1ed6ce85bfa0e7de17f6
Parker Barrett <parkerb7@vt.edu>
2023-07-15 14:18:48 -0400

    Add skeleton structure of matrix class

---
+++ include/Matrix.hpp
@@ -0,0 +1,25 @@
+// Matrix.hpp
+//
+// @Author: Parker Barrett
+// @Description: header only class implementation of matrix class.
+//
+
+// Include Headers
+#pragma once
+#include <cmath>
+#include <iostream>
+
+// Matrix Class
+class Matrix {
+  public:
+    /* @Matrix
+     * @Description: Matrix class constructor which sets values of
+     * matrix elements.
+     * @Inputs:
+     * @Outputs:
+     */
+    Matrix();
+
+  private:
+
+};
---
+++ test/MatrixTest.cpp
@@ -0,0 +1,11 @@
+// MatrixTest.hpp
+//
+// @Author: Parker Barrett
+// @Description: Matrix class test file.
+//
+
+// NavTypes Includes
+#include "Matrix.hpp"
+
+// External Includes
+#include "gtest/gtest.h"
