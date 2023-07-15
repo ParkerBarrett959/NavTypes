0048cb350f593af683f0000c491a9ae9031470d0
Parker Barrett <parkerb7@vt.edu>
2023-07-15 14:56:23 -0400

    Add constructors for matrix class and and functions to check rows and values at index

--- include/Matrix.hpp
+++ include/Matrix.hpp
@@ -8,6 +8,7 @@
 #pragma once
 #include <cmath>
 #include <iostream>
+#include <vector>
 
 // Matrix Class
 class Matrix {
@@ -16,10 +17,55 @@ class Matrix {
      * @Description: Matrix class constructor which sets values of
      * matrix elements.
      * @Inputs:
+     *     n_rows: Number of matrix rows
+     *     n_cols: Number of matrix columns
+     *     init_val: Initial value for matrix elements
      * @Outputs:
      */
-    Matrix();
+    Matrix(int n_rows, int n_cols, double init_val = 0.0) {
+        // Verify Input Dimensions are Valid
+        if (n_rows < 1 || n_cols < 1) {
+            return;
+        }
+
+        // Set Matriz Size and Values
+        n_rows_ = n_rows;
+        n_cols_ = n_cols;
+        m_.resize(n_rows);
+        for (int i = 0; i < m_.size(); i++) {
+            m_[i].resize(n_cols, init_val);
+        }
+    };
+
+    /* @cols
+     * @Description: Return number of matrix columns.
+     * @Inputs:
+     * @Outputs:
+     *     n_cols_: Number of matrix columns
+     */
+    int cols() { return n_cols_; };
+
+    /* @rows
+     * @Description: Return number of matrix rows.
+     * @Inputs:
+     * @Outputs:
+     *     n_rows_: Number of matrix rows
+     */
+    int rows() { return n_rows_; };
+
+    /* @()
+     * @Description: Return value of array given (row,col) index.
+     * @Inputs:
+     *     row: Row index
+     *     col: Column index
+     * @Outputs:
+     *     m_[row][col]: Value of matrix at row/column index
+     */
+    double operator()(int row, int col) { return m_[row][col]; };
 
   private:
-
+    // Matrix Class Members
+    int n_rows_ = 0;
+    int n_cols_ = 0;
+    std::vector<std::vector<double>> m_;
 };
--- test/MatrixTest.cpp
+++ test/MatrixTest.cpp
@@ -9,3 +9,39 @@
 
 // External Includes
 #include "gtest/gtest.h"
+
+// Test Constructor - No Args
+TEST(Constructor, NoArgs) {
+    // Construct Matrix
+    Matrix m(2, 3);
+
+    // Size Assertions
+    EXPECT_EQ(m.rows(), 2);
+    EXPECT_EQ(m.cols(), 3);
+
+    // Value Assertions
+    EXPECT_EQ(m(0, 0), 0.0);
+    EXPECT_EQ(m(0, 1), 0.0);
+    EXPECT_EQ(m(0, 2), 0.0);
+    EXPECT_EQ(m(1, 0), 0.0);
+    EXPECT_EQ(m(1, 1), 0.0);
+    EXPECT_EQ(m(1, 2), 0.0);
+}
+
+// Test Constructor - Args
+TEST(Constructor, Args) {
+    // Construct Matrix
+    Matrix m(2, 3, 1.5);
+
+    // Size Assertions
+    EXPECT_EQ(m.rows(), 2);
+    EXPECT_EQ(m.cols(), 3);
+
+    // Value Assertions
+    EXPECT_EQ(m(0, 0), 1.5);
+    EXPECT_EQ(m(0, 1), 1.5);
+    EXPECT_EQ(m(0, 2), 1.5);
+    EXPECT_EQ(m(1, 0), 1.5);
+    EXPECT_EQ(m(1, 1), 1.5);
+    EXPECT_EQ(m(1, 2), 1.5);
+}
